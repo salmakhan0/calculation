d41ed58c3f4db67f3c568726b36d939516fa3c91
Salma Khan <74590990+salmakhan0@users.noreply.github.com>
2022-09-20 01:21:52 +0600

    Add files via upload

--- index.js
+++ index.js
@@ -90,8 +90,14 @@ class Calculator {
     }
 const numberButtons = document.querySelectorAll('[data-number]')
 const operationButtons = document.querySelectorAll('[data-operation]')
-const equalsButton = document.querySelectorAll('[data-equals]')
-const numberButtons = document.querySelectorAll('[data-number]')
-const numberButtons = document.querySelectorAll('[data-number]')
+const equalsButton = document.querySelector('[data-equals]')
+const deleteButton = document.querySelector('[data-delete]')
+const alClearButton = document.querySelector('[data-all-clear]')
+const previousOperandTextElement = document.querySelector('[data-previous-operand]')
+const currentOperandTextElement = document.querySelector('[data-current-operand]')
+
+const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
+
+
 
     
