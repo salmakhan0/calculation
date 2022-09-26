0de55cfe14c73221ec305291958a7a9ed40ae2ea
Salma Khan <74590990+salmakhan0@users.noreply.github.com>
2022-09-27 00:51:48 +0600

    Add files via upload

--- index.js
+++ index.js
@@ -92,7 +92,7 @@ const numberButtons = document.querySelectorAll('[data-number]')
 const operationButtons = document.querySelectorAll('[data-operation]')
 const equalsButton = document.querySelector('[data-equals]')
 const deleteButton = document.querySelector('[data-delete]')
-const alClearButton = document.querySelector('[data-all-clear]')
+const allClearButton = document.querySelector('[data-all-clear]')
 const previousOperandTextElement = document.querySelector('[data-previous-operand]')
 const currentOperandTextElement = document.querySelector('[data-current-operand]')
 
@@ -100,4 +100,31 @@ const calculator = new Calculator(previousOperandTextElement, currentOperandText
 
 
 
-    
+numberButtons.forEach(button => {
+    button.addEventListener('click',() => {
+        calculator.appendNumber(button.innerText)
+        calculator.updateDisplay()
+    })
+} )  
+
+operationButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.chooseOperation(button.innerHTML)
+        calculator.
+    })
+})
+
+equalsButton.addEventListener('click',button => {
+    calculator.compute()
+    calculator.updateDisplay()
+})
+
+allClearButton.addEventListener('click', button => {
+    calculator.clear()
+    calculator.updateDisplay()
+})
+
+deleteButton.addEventListener('click', button => {
+    calculator.delete()
+    calculator.updateDisplay()
+})
